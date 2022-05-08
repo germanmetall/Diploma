5da41d41571e1e91ed74358a873a60e5d856de7c
Herman Mossur <pro-herman@ukr.net>
2022-05-08 15:26:32 +0300

    some improvements

--- front/front-platform/src/views/Profile.vue
+++ front/front-platform/src/views/Profile.vue
@@ -2,11 +2,13 @@
     <div class="page">
   		<Header></Header>
 		<main class="main">
-			<span class="heading heading--medium">В разработке</span>
             <section class="profile">
-                <span class="profile__name"></span>
-                <img class="profile__img" src=""/>
-                <span class="profile__contacts"></span>
+                <span class="profile__name">{{profile.name}}</span>
+                <div class="profile__img">
+					<img class="img__top" src="https://www.svgrepo.com/show/1001/graduation-cap.svg"/>
+					<img class="img__avatar" :src="profile.img"/>
+				</div>
+                <span class="profile__contacts">{{profile.contacts}}</span>
             </section>
 
 		</main>
@@ -22,7 +24,12 @@ export default {
 	},
 	data: function() {
 		return {
-			texts: undefined
+			texts: undefined,
+			profile: {
+				name: "Test name",
+				img: "https://pbs.twimg.com/media/FSEQED7X0AQQMKd?format=jpg&name=900x900",
+				contacts: "Test contacts contacts contacts contacts contacts contacts contacts contacts contacts contacts contacts"
+			}
 		};
 	},
 	mounted: async function() {
@@ -31,3 +38,46 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../../../styles/global.scss";
+.profile{
+	display: grid;
+	grid-template-columns: calc(30% - 6px) calc(70% - 6px);
+	gap: 12px;
+	&__name{
+		margin: auto;
+		font-size: 3rem;
+	}
+	&__img{
+		position: relative;
+		width: max(100px, 100%);
+		grid-row: 1 / span 2;
+		&:hover{
+			.img__top{
+				opacity: 1;
+				width: 80%;
+			}
+		}
+		.img{
+			&__top{
+				opacity: 0;
+				position: relative;
+				margin-bottom: -36%;
+				width: 5%;
+				transition: 1s;
+			}
+			&__avatar{
+				width: 100%;
+				aspect-ratio: 1;
+				object-fit: cover;
+				border-radius: 100%;
+			}
+		}
+	}
+	&__contacts{
+		text-align: start;
+		font-size: 1.75rem;
+	}
+}
+</style>
